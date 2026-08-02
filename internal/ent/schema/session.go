21c0d985eeef9322872d4a598119445d6f666ce1
abdullahsafwan <abdullahsafwan768@gmail.com>
2026-08-02 21:31:58 +0800

    Define Session and User schemas with fields and edges

--- internal/ent/schema/session.go
+++ internal/ent/schema/session.go
@@ -1,6 +1,12 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
 
 // Session holds the schema definition for the Session entity.
 type Session struct {
@@ -9,10 +15,18 @@ type Session struct {
 
 // Fields of the Session.
 func (Session) Fields() []ent.Field {
-	return nil
+	id := field.String("id").NotEmpty().Immutable()
+	expires_at := field.Time("expires_at").Immutable()
+	last_seen := field.Time("last_seen").Default(time.Now)
+	ip_address := field.String("ip_address").NotEmpty()
+	user_agent_hash := field.String("user_agent_hash").NotEmpty()
+	created_at := field.Time("created_at").Default(time.Now).Immutable()
+
+	return []ent.Field{id, expires_at, last_seen, ip_address, user_agent_hash, created_at}
 }
 
 // Edges of the Session.
 func (Session) Edges() []ent.Edge {
-	return nil
+	edge := edge.From("owner", User.Type).Ref("sessions").Unique().Required()
+	return []ent.Edge{edge}
 }
